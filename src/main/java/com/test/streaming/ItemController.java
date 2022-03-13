a57fd9185115314428d376d3fed4e6c99fc95160
Alexandre.B <alexandrebechet@hotmail.fr>
2022-03-13 22:49:59 +0100

    Modification de l'application pour démarrage

---
+++ src/main/java/com/test/streaming/ItemController.java
@@ -0,0 +1,13 @@
+package com.test.streaming;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/item")
+    public String getItem(){
+        return "Item selected";
+    }
+}
